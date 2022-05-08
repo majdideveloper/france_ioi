03224ed881bb54ecb96b24c448d755f3e45f6a60
majdi.developer <majdi.developer@gmail.com>
2022-05-08 22:29:10 +0100

    :technologist: commit in list :technologist:

--- Niveau 2/2-list/course_trois_jambes.c
+++ Niveau 2/2-list/course_trois_jambes.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 int compareEntier(const void *pEntier1, const void *pEntier2)
 {
-    return (*(int *)(pEntier1) - *(int *)(pEntier2));
+   return (*(int *)(pEntier1) - *(int *)(pEntier2));
 }
 int main()
 {
-    int number, minNumber;
-    int value;
+    int number,midValue;
+     int value;
     scanf("%d", &number);
     int tabNumber[number];
     for (int i = 0; i < number; i++)
     {
-        scanf("%d", &value);
-        tabNumber[i] = value;
+        scanf("%d",&value);
+        tabNumber[i]=value;
     }
     qsort(tabNumber, number, sizeof(int), compareEntier);
-    for (int i = 0; i <= (number / 2); i++)
+    if (number/2==0)
     {
-        if ((number / 2) % 2 == 0)
-        {
-            printf("%d %d\n", tabNumber[i], tabNumber[(number - 1)]);
-            number--;
-        }
-        else
-        {
-            printf("%d %d\n", tabNumber[i], tabNumber[(number - 1)]);
-            number--;
-        }
+    midValue=(number/2);
     }
+    else{
+    midValue=(number/2)-1
+    }
+    for(int i =0 ; i<=midValue;i++){
+    
+          printf("%d %d\n",tabNumber[i],tabNumber[(number-1)]);
+          number--;
+    }
+  
 }
