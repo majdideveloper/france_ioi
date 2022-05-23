6a8cff68d42b076be251faa2cbd8002a21d0efd1
majdi.developer <majdi.developer@gmail.com>
2022-05-23 15:56:00 +0100

    level 3 first file

---
+++ level3/emprunts_livres.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int void (  ){
+	int nbLivres,nbJours,nbClients;
+	scanf("%d %d",&nbLivres,&nbJours);
+	for(int i=0 ; i<nbJours;i++){
+int indiceLivre,dureLivre;
+scanf("%d %d",&indiceLivre,&dureLivre);
+
+
+	}
+		
+
+}
