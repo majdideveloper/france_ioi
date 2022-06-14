fc8bb2b885ccbffd92aa225b43cd14cf0b10087f
majdi.developer <majdi.developer@gmail.com>
2022-06-14 22:33:52 +0100

    try to find solution for task 2

---
+++ level3/flechettes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Nothing
+ */
+int main(void)
+{
+	int numCara, numberCarInLine;
+
+	scanf("%d", &numCara);
+
+	numberCarInLine = (numCara * 2) - 1;
+	for (int i = 0; i < numberCarInLine; i++)
+	{
+		
+	}
+
+}
